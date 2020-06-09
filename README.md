16b0b5e59bce73cc446a64b696bea100483bc888
Felix Chen <felixchen1998@gmail.com>
2020-06-08 21:33:09 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # screen-share
+- https://screen-share.netlify.app/, no need to install or run anything
+- application to stream screens between users, audio works for Chromium tabs
+
+## using
+- peer to peer system, using [Peer.js](https://peerjs.com/) (WebRTC wrapper)
+- front-end created with [IBM Carbon Design](https://www.carbondesignsystem.com/)
+
+## motivation
+- watch videos together with friends 
+- show friends webpages
+- use peer to peer
+
+
+## takeaway
+   - Peer to peer applications <ins>don't need me to write or host a server</ins>
+   - Browsers have powerful API's ([Media Stream](https://developer.mozilla.org/en-US/docs/Web/API/MediaStream), etc.)
+   - WebRTC is extremely powerful for browser applications
+   - IBM Carbon Design looks good
---
+++ index.js
@@ -0,0 +1,139 @@
+// let peer = new Peer({
+//     "host": '127.0.0.1',
+//     "port": "80"
+// });
+
+let peer = new Peer();
+
+let screenStream = null
+
+let emptyAudioTrack = null;
+let emptyVideoTrack = null;
+let emptyMediaStream = null;
+
+let video = null;
+
+/////////////////////////////////////////////
+// Frontend 
+/////////////////////////////////////////////
+$(function () {
+    video = document.getElementById("media");
+
+    $('#peerIDCopy').click(() => {
+        let peerID = document.getElementById('peerID')
+        text = peerID.textContent
+
+        let e = document.createElement("textarea")
+        e.value = text
+
+        document.body.appendChild(e)
+        e.select()
+        document.execCommand("copy")
+        document.body.removeChild(e)
+    })
+
+    $('#startWatching').click(() => {
+        let streamerID = $('#streamerID').val()
+        getScreenStream(streamerID);
+    })
+});
+
+
+/////////////////////////////////////////////
+// Peers 
+/////////////////////////////////////////////
+peer.on('open', function (id) {
+    $('#peerID').text(id);
+    $('#loading').hide();
+});
+
+peer.on('call', function (call) {
+    call.answer(screenStream);
+});
+
+/////////////////////////////////////////////
+// Actions
+/////////////////////////////////////////////
+function getScreenStream(peerID) {
+    // Connect with empty media stream
+    if (emptyMediaStream == null) {
+        emptyAudioTrack = createEmptyAudioTrack();
+        emptyVideoTrack = createEmptyVideoTrack({
+            width: 1920,
+            height: 1080
+        });
+        emptyMediaStream = new MediaStream([emptyAudioTrack, emptyVideoTrack])
+    }
+
+    call = peer.call(peerID, emptyMediaStream);
+    call.on('stream', function (stream) {
+        console.log('Got stream ', stream)
+        video.srcObject = stream
+        video.muted = false
+        video.controls = true
+    });
+}
+
+/////////////////////////////////////////////
+// Helpers
+/////////////////////////////////////////////
+function createEmptyAudioTrack() {
+    let ctx = new AudioContext();
+    let oscillator = ctx.createOscillator();
+    let dst = oscillator.connect(ctx.createMediaStreamDestination());
+    oscillator.start();
+    let track = dst.stream.getAudioTracks()[0];
+    return Object.assign(track, {
+        enabled: false
+    });
+};
+
+function createEmptyVideoTrack({
+    width,
+    height
+}) {
+    let canvas = Object.assign(document.createElement('canvas'), {
+        width,
+        height
+    });
+    canvas.getContext('2d').fillRect(0, 0, width, height);
+
+    let stream = canvas.captureStream();
+    let track = stream.getVideoTracks()[0];
+
+    return Object.assign(track, {
+        enabled: false
+    });
+};
+
+function setScreenStream() {
+
+    if (screenStream != null) {
+        screenStream.getTracks().forEach((mediaStreamTrack) => {
+            mediaStreamTrack.stop();
+        })
+    }
+
+    let options = {
+        video: {
+            width: 1920,
+            height: 1080,
+            frameRate: 60
+        },
+        audio: {
+            autoGainControl: false,
+            googAutoGainControl: false,
+            echoCancellation: false,
+            noiseSuppression: false,
+            sampleRate: 44100
+        }
+    }
+
+    navigator.mediaDevices.getDisplayMedia(options).then(function (stream) {
+            console.log('Created display media', stream.get)
+            screenStream = stream
+        })
+        .catch(function (err) {
+            console.log("Error when calling with screen media stream")
+        });
+}
