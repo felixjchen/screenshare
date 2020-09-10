00b461d2bff10f8840eae8d1383245831e5ccaa2
Felix Chen <felixchen1998@gmail.com>
2020-09-10 19:13:22 -0400

    refactor

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let peer = new Peer({
-    host: "peer-server.azurewebsites.net",
-    port: 443,
-    path: "/myapp",
-    secure: true,
+  host: "peer-server.azurewebsites.net",
+  port: 443,
+  path: "/myapp",
+  secure: true,
 });
 
 // my screenshare stream
 let stream = null;
 // my video element
-let watchers = 0;
 let video = null;
 
 let emptyAudioTrack = null;
@@ -18,173 +17,177 @@ let emptyMediaStream = null;
 /////////////////////////////////////////////
 // Frontend
 /////////////////////////////////////////////
-$(function() {
-    video = document.getElementById("media");
+$(function () {
+  video = document.getElementById("media");
 
-    $("#peerIDCopy").click(() => {
-        let peerID = document.getElementById("peerID");
-        text = peerID.textContent;
+  $("#peerIDCopy").click(() => {
+    let peerID = document.getElementById("peerID");
+    text = peerID.textContent;
 
-        let e = document.createElement("textarea");
-        e.value = text;
+    let e = document.createElement("textarea");
+    e.value = text;
 
-        document.body.appendChild(e);
-        e.select();
-        document.execCommand("copy");
-        document.body.removeChild(e);
-    });
+    document.body.appendChild(e);
+    e.select();
+    document.execCommand("copy");
+    document.body.removeChild(e);
+  });
 
-    $("#streamerID").keyup(function() {
-        getStream();
-    });
+  $("#streamerID").keyup(function () {
+    watchStream();
+  });
 });
 
 /////////////////////////////////////////////
 // Peer Listeners
 /////////////////////////////////////////////
-peer.on("open", function(id) {
-    $("#peerID").text(id);
-    $("#loading").hide();
+peer.on("open", function (id) {
+  $("#peerID").text(id);
+  $("#loading").hide();
 });
 
-peer.on("call", function(mediaConnection) {
-    mediaConnection.answer(stream);
-
-    watchers++;
-    mediaConnection.on("close", () => {
-        watchers--;
-    });
+peer.on("call", function (mediaConnection) {
+  mediaConnection.answer(stream);
 });
 
 /////////////////////////////////////////////
 // Actions
 /////////////////////////////////////////////
-let getStream = () => {
-    // If not empty media stream
-    if (emptyMediaStream == null) setEmptyMediaStream();
+let watchStream = () => {
+  // If not empty media stream
+  if (emptyMediaStream == null) setEmptyMediaStream();
 
-    // Get streamer ID from input field
-    let streamerID = $("#streamerID").val();
+  // Get streamer ID from input field
+  let streamerID = $("#streamerID").val();
 
-    mediaConnection = peer.call(streamerID, emptyMediaStream);
-    mediaConnection.on("close", () => {
-        stopVideo();
-    });
+  mediaConnection = peer.call(streamerID, emptyMediaStream);
 
-    mediaConnection.on("stream", function(stream) {
-        startVideo(stream);
-    });
+  mediaConnection.on("stream", function (stream) {
+    startVideo(stream);
+  });
+  mediaConnection.on("close", () => {
+    stopVideo();
+  });
 };
 
-let setStream = async() => {
-    // Audio suggestions: https://stackoverflow.com/questions/46063374/is-it-really-possible-for-webrtc-to-stream-high-quality-audio-without-noise
-    let options = {
-        video: {
-            width: { ideal: 1920 },
-            height: { ideal: 1080 },
-            frameRate: 60,
-        },
-        audio: {
-            autoGainControl: false,
-            channelCount: 2,
-            echoCancellation: false,
-            latency: 0,
-            noiseSuppression: false,
-            sampleRate: 48000,
-            sampleSize: 16,
-        },
-    };
+let startStreaming = async () => {
+  // Audio suggestions: https://stackoverflow.com/questions/46063374/is-it-really-possible-for-webrtc-to-stream-high-quality-audio-without-noise
+  let options = {
+    video: {
+      width: { ideal: 1920 },
+      height: { ideal: 1080 },
+      frameRate: 60,
+    },
+    audio: {
+      autoGainControl: false,
+      channelCount: 2,
+      echoCancellation: false,
+      latency: 0,
+      noiseSuppression: false,
+      sampleRate: 48000,
+      sampleSize: 16,
+    },
+  };
 
-    try {
-        stream = await navigator.mediaDevices.getDisplayMedia(options);
-    } catch (e) {
-        console.log("Error on setting stream: ", e);
-    }
+  try {
+    stream = await navigator.mediaDevices.getDisplayMedia(options);
 
-    startVideo(stream);
-    $("#media").css("border", "2px solid #da1e28");
+    startStreamVideo(stream);
 
     // I've stopped streaming, attach listener to the stream
     // https://stackoverflow.com/questions/25141080/how-to-listen-for-stop-sharing-click-in-chrome-desktopcapture-api
     stream.getVideoTracks()[0].onended = () => {
-        stopStream();
+      stopStreaming();
     };
+  } catch (e) {
+    console.log("Error on setting stream: ", e);
+  }
 };
 
-let stopStream = () => {
-    if (stream) {
-        // Stop showing my stream
-        stopVideo();
+let stopStreaming = () => {
+  if (stream) {
+    // Stop showing my stream
+    stopVideo();
 
-        // Stop stream, then dereference it
-        stream.getTracks().forEach((track) => {
-            track.stop();
-        });
-        stream = null;
+    // Stop stream, then dereference it
+    stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+    stream = null;
 
-        // Stop all connections watching this stream
-        for (let peerID in peer.connections) {
-            conns = peer.connections[peerID];
-            conns.forEach((i) => {
-                i.close();
-            });
-        }
+    // Stop all connections watching this stream
+    for (let peerID in peer.connections) {
+      conns = peer.connections[peerID];
+      conns.forEach((i) => {
+        i.close();
+      });
     }
+  }
 };
 
 /////////////////////////////////////////////
 // Helpers
 /////////////////////////////////////////////
-let stopVideo = () => {
-    video.srcObject = null;
-    video.controls = false;
-    video.style = null;
+let startStreamVideo = (stream) => {
+  video.srcObject = stream;
+
+  video.onloadeddata = () => {
+    video.style.border = "3px solid red";
+    video.style.height = "auto";
+    video.play();
+  };
 };
 let startVideo = (stream) => {
-    video.srcObject = stream;
+  video.srcObject = stream;
 
-    video.onloadeddata = () => {
-        video.muted = false;
-        video.controls = true;
-        video.style.height = "auto";
-        video.play();
-    };
+  video.onloadeddata = () => {
+    video.muted = false;
+    video.controls = true;
+    video.style.height = "auto";
+    video.play();
+  };
+};
+let stopVideo = () => {
+  video.srcObject = null;
+  video.controls = false;
+  video.style = null;
+  video.muted = true;
 };
 
 /////////////////////////////////////////////
 // Empty Streams
 /////////////////////////////////////////////
 let createEmptyAudioTrack = () => {
-    let ctx = new AudioContext();
-    let oscillator = ctx.createOscillator();
-    let dst = oscillator.connect(ctx.createMediaStreamDestination());
-    oscillator.start();
-    let track = dst.stream.getAudioTracks()[0];
-    return Object.assign(track, {
-        enabled: false,
-    });
+  let ctx = new AudioContext();
+  let oscillator = ctx.createOscillator();
+  let dst = oscillator.connect(ctx.createMediaStreamDestination());
+  oscillator.start();
+  let track = dst.stream.getAudioTracks()[0];
+  return Object.assign(track, {
+    enabled: false,
+  });
 };
 
 let createEmptyVideoTrack = ({ width, height }) => {
-    let canvas = Object.assign(document.createElement("canvas"), {
-        width,
-        height,
-    });
-    canvas.getContext("2d").fillRect(0, 0, width, height);
+  let canvas = Object.assign(document.createElement("canvas"), {
+    width,
+    height,
+  });
+  canvas.getContext("2d").fillRect(0, 0, width, height);
 
-    let stream = canvas.captureStream();
-    let track = stream.getVideoTracks()[0];
+  let stream = canvas.captureStream();
+  let track = stream.getVideoTracks()[0];
 
-    return Object.assign(track, {
-        enabled: false,
-    });
+  return Object.assign(track, {
+    enabled: false,
+  });
 };
 
 let setEmptyMediaStream = () => {
-    emptyAudioTrack = createEmptyAudioTrack();
-    emptyVideoTrack = createEmptyVideoTrack({
-        width: 600,
-        height: 400,
-    });
-    emptyMediaStream = new MediaStream([emptyAudioTrack, emptyVideoTrack]);
+  emptyAudioTrack = createEmptyAudioTrack();
+  emptyVideoTrack = createEmptyVideoTrack({
+    width: 600,
+    height: 400,
+  });
+  emptyMediaStream = new MediaStream([emptyAudioTrack, emptyVideoTrack]);
 };
