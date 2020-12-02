c482fdc8507f023e248791e904d1242383c47f0f
Felix Chen <felixchen1998@gmail.com>
2020-12-01 23:05:36 -0500

    start stream

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -22,30 +22,66 @@ import { FunctionComponent, useState, useEffect, useRef } from "react";
 import { Recording20 } from "@carbon/icons-react";
 import { Stream } from "./stream";
 
-const Page: FunctionComponent = () => (
-  <div id="page">
-    <HeaderContainer
-      render={() => (
-        <>
-          <Header aria-label="IBM Platform Name">
-            <HeaderName href="#" prefix="Stream">
-              Share
-            </HeaderName>
-            <HeaderNavigation aria-label="Stream"></HeaderNavigation>
-            <HeaderGlobalBar>
-              <HeaderGlobalAction id="record_icon" aria-label="Logout">
-                <Recording20 />
-              </HeaderGlobalAction>
-            </HeaderGlobalBar>
-          </Header>
-        </>
-      )}
-    />
+const Page: FunctionComponent = () => {
+  const [stream, setStream] = useState(undefined);
 
-    <div id="stream_container">
-      <Stream></Stream>
+  const startStream = async () => {
+    // Audio suggestions: https://stackoverflow.com/questions/46063374/is-it-really-possible-for-webrtc-to-stream-high-quality-audio-without-noise
+    const options = {
+      video: {
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+        frameRate: 60,
+      },
+      audio: {
+        autoGainControl: false,
+        channelCount: 2,
+        echoCancellation: false,
+        latency: 0,
+        noiseSuppression: false,
+        sampleRate: 48000,
+        sampleSize: 16,
+      },
+    };
+
+    try {
+      const mediaDevices = navigator.mediaDevices as any;
+      const stream = await mediaDevices.getDisplayMedia(options);
+      setStream(stream);
+    } catch (e) {
+      console.log("Error on starting stream: ", e);
+    }
+  };
+
+  return (
+    <div id="page">
+      <HeaderContainer
+        render={() => (
+          <>
+            <Header aria-label="IBM Platform Name">
+              <HeaderName href="#" prefix="Stream">
+                Share
+              </HeaderName>
+              <HeaderNavigation aria-label="Stream"></HeaderNavigation>
+              <HeaderGlobalBar>
+                <HeaderGlobalAction
+                  id="record_icon"
+                  aria-label="Logout"
+                  onClick={startStream}
+                >
+                  <Recording20 />
+                </HeaderGlobalAction>
+              </HeaderGlobalBar>
+            </Header>
+          </>
+        )}
+      />
+
+      <div id="stream_container">
+        <Stream></Stream>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { Page };
