2d2b3230a47f4f9fcea6006ccd8335676b8d815e
Felix Chen <felixchen1998@gmail.com>
2020-12-02 22:40:17 -0500

    legacy copy to clipboard

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -9,14 +9,27 @@ const setVh = () => {
 };
 
 const copyToClipboard = (str: string) => {
-  navigator.clipboard.writeText(str).then(
-    function () {
-      console.log("Async: Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Async: Could not copy text: ", err);
-    }
-  );
+  const textArea = document.createElement("textarea");
+  textArea.value = str;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    const successful = document.execCommand("copy");
+    const msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
 };
 
 ////// URL Create/Parse
