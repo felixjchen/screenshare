bd0f7f0f8ca31127d34692da7047f52015e692b3
Felix Chen <felixchen1998@gmail.com>
2020-12-03 19:55:30 -0500

    remove videojs

--- src/components/stream.tsx
+++ src/components/stream.tsx
@@ -1,67 +1,20 @@
-import videojs from "video.js";
+// import videojs from "video.js";
 import { FunctionComponent, useState, useEffect, useRef } from "react";
-
-const videoJSOptions = {
-  autoplay: true,
-  controls: true,
-  inactivityTimeout: 0,
-  userActions: { hotkeys: true },
-  controlBar: {
-    progressControl: false,
-    remainingTimeDisplay: false,
-  },
-  bigPlayButton: false,
-} as any;
+import ReactPlayer from "react-player";
 
 type StreamProps = {
   stream: MediaStream | undefined;
 };
 
 const Stream: FunctionComponent<StreamProps> = ({ stream }) => {
-  const videoPlayerRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    if (videoPlayerRef) {
-      const player = videojs(videoPlayerRef.current, videoJSOptions, () => {
-        // const ButtonClass = videojs.getComponent("Button");
-        // const RecordButtonClass = videojs.extend(ButtonClass, {
-        //   constructor: function () {
-        //     ButtonClass.call(this, player);
-        //   }, // notice the comma
-
-        //   handleClick: function () {
-        //     // Do your stuff
-        //     console.log(1);
-        //   },
-        // });
-        // const RecordButton = player.controlBar.addChild(
-        //   new RecordButtonClass()
-        // );
-
-        // RecordButton.addClass("vjs-record-button");
-        // RecordButton.el_.firstChild.classList.add(
-        //   "vjs-icon-circle-inner-circle"
-        // );
-
-        // Test video
-        // console.log(stream);
-        console.log();
-        if (stream !== undefined) {
-          const videoElement: any = player.tech().el();
-          videoElement.srcObject = stream;
-          console.log(player.tech());
-        }
-        console.log(player);
-      });
-    }
-
-    return () => {};
-  });
-
-  return (
-    <div data-vjs-player>
-      <video ref={videoPlayerRef} className="video-js vjs-fill"></video>
-    </div>
-  );
+  // const videoPlayerRef = useRef<HTMLVideoElement>(null);
+  const reactPlayerProps = {
+    url: stream,
+    controls: true,
+    width: "100%",
+    height: "100%",
+  };
+  return <ReactPlayer {...reactPlayerProps} />;
 };
 
 export { Stream };
